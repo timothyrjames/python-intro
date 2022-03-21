6996f21e911d3cbaba00102cf53de84531302ed5
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-03-21 14:43:34 -0400

    Create addition.py

---
+++ part01/addition.py
@@ -0,0 +1,11 @@
+# Simple Addition in Python
+
+print(1 + 1)
+
+# We can add several numbers together in a single statement.
+# We do not have to enter them one by one. 
+
+print(1 + 1 + 1)
+print(1 + 2 + 3 + 4)
+print(1 + 10 + 100 + 1000 + 10000)
+
