afb2e80890e2eaae375745702c07fe25b25f6f13
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-03-21 14:44:06 -0400

    Create arithmetic.py

---
+++ part01/arithmetic.py
@@ -0,0 +1,36 @@
+# Arithmetic in Python
+
+print(100 - 23 + 7 - 4)
+
+
+# If you want to change the order of operations, you can enclose parts of the 
+# calculation in parentheses. 
+
+print(100 - (23 + 7) - 4)
+
+
+# We can multiply using an asterisk.
+
+print(33.2 * 12.5)
+
+
+# Different arithmetic operations can be mixed.
+# Note that multiplication occurs before addition & subtraction, so ordering
+# matters.
+
+print(2 * 10 - 4 + 3 * 6)
+
+
+# Division uses a forward slash.
+
+print(40 / 3)
+
+
+# If we want to use integer division (also called floor division), we use a
+# double forward slash.
+
+print(40 // 3)
+
+print(50 / 11)
+print(50 // 11)
+
