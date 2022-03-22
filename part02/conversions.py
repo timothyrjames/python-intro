d412007c34d3cf6334aa038ff476a3d4c9d7ff6a
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-03-22 14:51:10 -0400

    Create conversions.py

---
+++ part02/conversions.py
@@ -0,0 +1,20 @@
+# You can convert a float to an int.
+
+x = 3.7
+print(int(x))
+
+# You can convert a string to a float.
+
+y = '97.3'
+print(float(y) + 2)
+
+# You can convert an int to a string.
+
+z = 3
+print('one possible number is ' + str(z))
+
+# Usually we wouldn't concatenate strings like that, though. We'd use
+
+print('one possible number is %s' % z)
+
+
