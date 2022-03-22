93defade40907f57a0a17e195aec60e03026bb3b
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-03-22 14:50:43 -0400

    Create comments.py

---
+++ part02/comments.py
@@ -0,0 +1,19 @@
+# Most comments in Python start w/ an octothorpe (also known as the pound sign).
+
+''' Multi-line comments in Python 
+    can be created with 3 single quotes '''
+
+
+""" 
+  Three
+  double
+  quotes
+  is
+  also
+  ok
+"""
+
+# Note that if you run this code, nothing will happen - because Python ignores
+# comments!
+
+
