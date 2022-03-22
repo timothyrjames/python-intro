1bf0809e123f56e16f52c76a2eba3922b39dadab
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-03-22 14:52:03 -0400

    Create variables.py

---
+++ part02/variables.py
@@ -0,0 +1,20 @@
+# To use a variable, we just give it a name and assign it a value.
+
+number = 4
+
+print(number)
+
+# Variable names are case sensitive.
+
+value = 50
+Value = 50
+
+print(value)
+print(Value)
+
+# Variables can also be used to perform calculations
+
+number_five = 5
+one_more_than_five = number_five + 1
+print(one_more_than_five)
+
