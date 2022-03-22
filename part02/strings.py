bc3044f8b47a9b30049dedf66eaa94d5be058b14
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-03-22 14:51:27 -0400

    Create strings.py

---
+++ part02/strings.py
@@ -0,0 +1,22 @@
+# We can use double quotes or single quotes.
+
+r = 'Single quote string.'
+s = "Double quote string."
+
+print(r)
+print(s)
+
+# A len function allows us to get the length of a string.
+
+r = 'abc'
+print(len(r))
+s = 'abcdefghijklmnopqrstuvwxyz'
+print(len(s))
+
+# You can also have multi-line strings.
+
+multi_line = '''Some really long string.
+It can take up multiple lines.
+The multi-line comment is really just a string and that's just valid syntax.'''
+print(multi_line)
+
