922c653dc8507f5512a3123015311cf6edb289d5
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-03-22 14:50:57 -0400

    Create comparisons.py

---
+++ part02/comparisons.py
@@ -0,0 +1,30 @@
+# Trying some comparisons with Boolean values.
+
+print(3 > 5)
+print(12 / 2 == 6)
+print(12 / 3 != 4)
+print(7 <= 3)
+
+# Create some Boolean variables.
+
+m = True
+n = True
+p = False
+q = False
+
+print(n)
+
+# Try using some of the operators!
+
+print(m and p)
+print(m or p)
+print(m and n)
+print(p or q)
+
+# You can also negate Boolean values.
+
+print(not q)
+print(not n)
+print(m or not p)
+print(m and not n)
+
