f0c960207c650421a5f67b1a9edd050a0594ef78
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-03-23 17:32:24 -0400

    Create booleans.py

---
+++ part03/booleans.py
@@ -0,0 +1,17 @@
+# Set some Boolean values.
+
+value_a = True
+value_b = False
+
+print(value_a and value_b)
+print(value_a or value_b)
+
+# Try some numeric comparisons.
+
+number_1 = 100
+number_2 = 200
+
+print(number_1 > number_2)
+print(number_1 < number_2)
+print(number_1 == number_2)
+
