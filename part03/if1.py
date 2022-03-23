6a2d5b8173f199559df3c71af7a36219ae7c7113
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-03-23 17:32:39 -0400

    Create if1.py

---
+++ part03/if1.py
@@ -0,0 +1,7 @@
+# Show our first if statement.
+
+something = input('Type something.')
+
+if something != "something":
+    # note the use of whitespace here; we're indenting these lines 4 spaces.
+    print("You didn't type 'something'!")
