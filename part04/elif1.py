c0e9e69539c7ec49609ebe6fb3750e7cace30ad8
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-03-24 15:22:27 -0400

    Create elif1.py

---
+++ part04/elif1.py
@@ -0,0 +1,11 @@
+num = int(input('Guess a number between 1 and 4.'))
+
+if num == 1:
+    print('1 is not the answer.')
+elif num == 2:
+    print('You guessed it!')
+elif num == 3:
+    print("It wasn't 3.")
+elif num == 4:
+    print('It was not 4.')
+
