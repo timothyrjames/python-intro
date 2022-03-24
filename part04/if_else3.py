612402537c894b19d7e7eb0faf94dc0a3f06b9b8
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-03-24 15:23:55 -0400

    Create if_else3.py

---
+++ part04/if_else3.py
@@ -0,0 +1,10 @@
+num = int(input('Enter a number.'))
+
+if num == 0:
+    print('That number is zero; it is not negative or positive.')
+else:
+    if num > 0:
+        print('That number is positive.')
+    else:
+        print('That number is negative.')
+
