9e9a87a97a5604c74e626f6981de906b20014a0f
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-03-24 16:42:11 -0400

    Create if_elif_else.py

---
+++ part04/if_elif_else.py
@@ -0,0 +1,12 @@
+# This example extends if_else2.py to illustrate when to use "elif"
+
+food = input('Guess my favorite food.')
+
+if food == 'pizza':
+    print("Pizza is the best!")
+elif food == 'tacos:'
+    # Because we're responding in 2 different ways, we use "elif"
+    print('I really like tacos.')
+else:
+    print("That's not it.")
+
