55c324a72abd96c9f9387db49f57c034df20565c
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-03-24 15:47:55 -0400

    fixed typo on line 5

--- part04/greatest1.py
+++ part04/greatest1.py
@@ -2,7 +2,7 @@ num1 = int(input('Enter one number.'))
 num2 = int(input('Enter another number.'))
 num3 = int(input('Enter one more number.'))
 
-if num1 >= num2 and num1 >= num2:
+if num1 >= num2 and num1 >= num3:
     print('%s is the greatest of those three numbers.' % num1)
 elif num2 >= num3 and num2 >= num1:
     print('%s is the greatest of those three numbers.' % num2)
