da84dfb3a3712e3c0f21ae9fe07f1204218e43c0
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-03-24 15:22:51 -0400

    Create greatest1.py

---
+++ part04/greatest1.py
@@ -0,0 +1,11 @@
+num1 = int(input('Enter one number.'))
+num2 = int(input('Enter another number.'))
+num3 = int(input('Enter one more number.'))
+
+if num1 >= num2 and num1 >= num2:
+    print('%s is the greatest of those three numbers.' % num1)
+elif num2 >= num3 and num2 >= num1:
+    print('%s is the greatest of those three numbers.' % num2)
+elif num3 >= num2 and num3 >= num1:
+    print('%s is the greatest of those three numbers.' % num3)
+
