7ecee30b4ab69a48aab38f31d15b0b2075065d48
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-03-29 16:28:51 -0400

    Create loop_else4.py

---
+++ part06/loop_else4.py
@@ -0,0 +1,11 @@
+my_string = '98765'
+
+print("Let's subtract 1 from each of %s." % my_string)
+for c in my_string:
+    if c == '7':
+        break
+    num_value = int(c)
+    print(num_value - 1)
+else:
+    print("That's all!")
+
