daa2aec8c23501e25ed976d9b2a2bf68b62ca82f
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-03-29 16:29:00 -0400

    Create nesting1.py

---
+++ part06/nesting1.py
@@ -0,0 +1,13 @@
+i = 1
+
+# Outer loop counts up to 8
+while i < 8:
+    k = i
+    i += 1
+
+    print('Countdown!')
+    # Inner loop counts down to 0
+    while k > 0:
+        print(k)
+        k -= 1
+
