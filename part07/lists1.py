d7cff3724aa681020e1e419d35af85e126e6fe7c
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-03-30 12:31:06 -0400

    Create lists1.py

---
+++ part07/lists1.py
@@ -0,0 +1,19 @@
+names = ["Kanye", "Eminem", "Jay-Z"]
+print('A list of names:')
+print(names)
+
+numbers = [10, 8, 14, 13, 4, 20, 1]
+print('A list of numbers:')
+print(numbers)
+
+# Lists don't have to contain only the same kind of thing;
+# they can contain ints, strings, bools, and floats.
+things = [7, 'thing', 3.7, True]
+print('List with different types:')
+print(things)
+
+# Lists can even contain other lists.
+list_list = [[1, 2, 3], [4.0, 5.0, 6.0], [True, False]]
+print('A list of lists:')
+print(list_list)
+
