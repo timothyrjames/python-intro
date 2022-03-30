66bf8242af35e64b6b4c089a82740b94517ae301
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-03-30 12:31:27 -0400

    Create string_slices.py

---
+++ part07/string_slices.py
@@ -0,0 +1,12 @@
+print('Iteration with a for loop:')
+w = 'word'
+for c in w:
+    print(c)
+
+print('Iteration with a while loop:')
+i = 0
+w = 'word'
+while i < len(w):
+    print(w[i])
+    i += 1
+
