21106571cf228d295b68d76920ee5982590155b4
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-03-31 15:06:58 -0400

    Create sets1.py

---
+++ part08/sets1.py
@@ -0,0 +1,21 @@
+# Creating a set.
+my_set = set()
+print('An empty set:')
+print(my_set)
+
+# We can create a set from a list.
+values = [1, 7, 9, 2, 1, 3, 4, 9]
+my_other_set = set(values)
+print()
+print('The values in a list:')
+print(values)
+print('The values when that list becomes a set:')
+print(my_other_set)
+
+# We can use the len() function to get the number of values.
+print()
+print('The number of values in the list:')
+print(len(values))
+print('The number of values in the set:')
+print(len(my_other_set))
+
