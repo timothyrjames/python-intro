e08cd7b2f56c50c8653bb9224656876ffe2a2bcc
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-03-31 15:05:13 -0400

    Create dictionaries1.py

---
+++ part08/dictionaries1.py
@@ -0,0 +1,18 @@
+# Creating a dictionary.
+dictionary = {}
+print('This is an empty dictionary:')
+print(dictionary)
+
+# Another (less common) way to create a dictionary.
+d = dict()
+print('This is another empty dictionary:')
+print(d)
+
+states = {}
+# Populating a dictionary.
+states['NY'] = 'New York'
+states['CA'] = 'California'
+
+print('A dictionary with values:')
+print(states)
+
