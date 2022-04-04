be20d90539e49d6165d033b91e2a3f7f1452b969
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-04-04 16:02:43 -0400

    Create f_max.py

---
+++ part09/f_max.py
@@ -0,0 +1,14 @@
+list_values = [2, 8, 1, 4, 1, 3, -3, 1, 0]
+max_value = max(list_values)
+print(max_value)
+
+other_values = [1, 2, 3, 4, 5.7, -1, -2, -3, -4.2, -5, 10.1, 10.2, 9, 8]
+print(max(other_values))
+
+values_set = set(list_values)
+print(max(values_set))
+
+print(max(7, 8, 9, 1, 2, 3, 6, 7, 8, 4, 5, 6))
+
+print(max('banana', 'date', 'cherry', 'grape', 'apple'))
+
