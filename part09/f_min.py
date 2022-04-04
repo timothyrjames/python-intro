903b14a455b8efa7087aa7f0085046c7ee9101e6
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-04-04 16:02:54 -0400

    Create f_min.py

---
+++ part09/f_min.py
@@ -0,0 +1,14 @@
+list_values = [2, 8, 1, 4, 1, 3, -3, 1, 0]
+min_value = min(list_values)
+print(min_value)
+
+other_values = [1, 2, 3, 4, 5.7, -1, -2, -3, -4.2, -5, 10.1, 10.2, 9, 8]
+print(min(other_values))
+
+values_set = set(list_values)
+print(min(values_set))
+
+print(min(7, 8, 9, 1, 2, 3, 6, 7, 8, 4, 5, 6))
+
+print(min('banana', 'date', 'cherry', 'grape', 'apple'))
+
