dd14edb1b5b242d5f84e2b41526e5012a8782854
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-04-07 15:24:22 -0400

    Create objects2.py

---
+++ part11/objects2.py
@@ -0,0 +1,30 @@
+my_list = [1, 2, 3, 4]
+
+print(my_list)
+print('Reversed:')
+my_list.reverse()
+print(my_list)
+print()
+
+my_list = [111, 222, 333, 111, 222, 111]
+print(my_list)
+print('Count the 111s:')
+print(my_list.count(111))
+print()
+
+my_list = [7,7,1,3,6,8,2,5,7,7,9]
+print(my_list)
+print('Sorted:')
+my_list.sort()
+print(my_list)
+print()
+
+my_list = []
+print(my_list)
+my_list.append('Cat')
+my_list.append('Fish')
+my_list.append('Dog')
+print('Appended:')
+print(my_list)
+print()
+
