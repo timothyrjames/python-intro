94008e833f45893c878bc1992c3e8f2f107fbe8c
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-04-11 16:35:41 -0400

    Create modules1.py

---
+++ part13/modules1.py
@@ -0,0 +1,16 @@
+import math
+
+print('Factorials: 3, 4, 5, and 6!')
+print(math.factorial(3))
+print(math.factorial(4))
+print(math.factorial(5))
+print(math.factorial(6))
+
+print()
+
+x = 3.9234
+print('The ceiling of %s is %s.' % (x, math.ceil(x)))
+print('The floor of %s is %s.' % (x, math.floor(x)))
+print('The natural log of %s is %s.' % (x, math.log(x)))
+print('The square root of %s is %s.' % (x, math.sqrt(x)))
+
