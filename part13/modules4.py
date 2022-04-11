b1bbd30e54cd5604b73329b076b3fcc64de2ed6d
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-04-11 16:36:17 -0400

    Create modules4.py

---
+++ part13/modules4.py
@@ -0,0 +1,8 @@
+import math as m
+
+angle = 90
+
+print('The sine of %s is %s.' % (angle, m.sin(angle)))
+print('The cosine of %s is %s.' % (angle, m.cos(angle)))
+print('The tangent of %s is %s.' % (angle, m.tan(angle)))
+
