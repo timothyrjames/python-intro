f43db717585fae8d5ebbeb271df887fac0a44af5
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-04-12 15:43:13 -0400

    Create problems2.py

---
+++ part14/problems2.py
@@ -0,0 +1,20 @@
+import random
+
+number = random.randint(0, 100) + 1
+guess = -1
+
+while guess != number:
+    user_input = input('Guess a number from 1 to 100. [Enter] to quit.')
+    if user_input:
+        guess = int(user_input)
+        if guess == number:
+            print('You got it!')
+        elif guess < number:
+            print('Your guess is too low.')
+        elif guess > number:
+            print('Your guess is too high.')
+    else:
+        break
+
+print('The number was %s.' % number)
+
